b9278e3f7756972be5ff1ba99800cabaf0d9a620
mchilty <mchilty@gmail.com>
2024-01-25 12:14:27 -0600

    More basic format and style cleanup after linting.

--- userstodos.py
+++ userstodos.py
@@ -1,22 +1,21 @@
 #!/usr/local/bin/python3
 #
+# mchilty@gmail.com
 #
-#mchilty@gmail.com 
+# Collects and harmonizes oddball Latin homework completion status using 2 different approaches from:
 #
-#Collects and harmonizes oddball Latin homework completion status using 2 different approaches from:
-#
-#https://jsonplaceholder.typicode.com/users
-#https://jsonplaceholder.typicode.com/todos
-#https://jsonplaceholder.typicode.com/users/1
-#
-#
-#Output example:
-#{"userName": "Bret", "email": "Sincere@april.biz", "todoTitle":"delectus aut autem", "completed": false}
-#
-#configure the proxy as suits your needs or remove related arguments
+# https://jsonplaceholder.typicode.com/users
+# https://jsonplaceholder.typicode.com/todos
+# https://jsonplaceholder.typicode.com/users/1
 #
 #
-
+# Output example:
+# {"userName": "Bret", "email": "Sincere@april.biz", "todoTitle":"delectus aut autem", "completed": false}
+#
+# Configure the proxy as suits your needs or remove related arguments
+#
+#
+"""import modules required to perform tasks"""
 import json
 import os
 import requests
@@ -43,76 +42,75 @@ print()
 
 
 #
-#follows recommended approach provided in challenge
-#uses data found in /todos and queries or fetches related data from the user's profile
+# follows recommended approach provided in challenge
+# uses data found in /todos and queries or fetches related data from the user's profile
 #
 todos_extract = {}
 for todo in todos_response_data:
-		userId = (todo['userId'])
-		title = (todo['title'])
-		completed = (todo['completed'])
-		users_bio_url = (users_url + "/" + str(todo['userId']))
-		users_bio_response = requests.get(users_bio_url, timeout=5, headers=headers, proxies=proxies) 
-		users_bio_response_data = users_bio_response.json()
+	userId = (todo['userId'])
+	title = (todo['title'])
+	completed = (todo['completed'])
+	users_bio_url = (users_url + "/" + str(todo['userId']))
+	users_bio_response = requests.get(users_bio_url, timeout=5, headers=headers, proxies=proxies)
+	users_bio_response_data = users_bio_response.json()
+
+	username = (users_bio_response_data['username'])
+	email = (users_bio_response_data['email'])
 		
-		username = (users_bio_response_data['username'])
-		email = (users_bio_response_data['email'])
-		
-		todos_extract['userName'] = username
-		todos_extract['email'] = email
-		todos_extract['todoTitle'] = title
-		todos_extract['completed'] = completed
-		todos_extract_json_data = json.dumps(todos_extract)	
-		print(todos_extract_json_data)
+	todos_extract['userName'] = username
+	todos_extract['email'] = email
+	todos_extract['todoTitle'] = title
+	todos_extract['completed'] = completed
+	todos_extract_json_data = json.dumps(todos_extract)
+	print(todos_extract_json_data)
 		
 print()
 print("################")
 print()
-	
+
 #
-#uses the data returned by /todos and /users without using subsequent API requests
+# uses the data returned by /todos and /users without using subsequent API requests
 #
 another_todos_extract = {}
 for user in users_response_data:
-		username = (user['username'])
-		email = (user['email'])
+	username = (user['username'])
+	email = (user['email'])
 
-		for todo in todos_response_data:
-				title = (todo['title'])
-				completed = (todo['completed'])
-				
-				if (todo['userId']) == (user['id']):
-					another_todos_extract['userName'] = username
-					another_todos_extract['email'] = email
-					another_todos_extract['todoTitle'] = title
-					another_todos_extract['completed'] = completed
-					another_todos_extract_json_data = json.dumps(another_todos_extract)	
-					print(another_todos_extract_json_data)
+	for todo in todos_response_data:
+		title = (todo['title'])
+		completed = (todo['completed'])
+			
+		if (todo['userId']) == (user['id']):
+			another_todos_extract['userName'] = username
+			another_todos_extract['email'] = email
+			another_todos_extract['todoTitle'] = title
+			another_todos_extract['completed'] = completed
+			another_todos_extract_json_data = json.dumps(another_todos_extract)
+			print(another_todos_extract_json_data)
 
 print()
 print("################")
-print()		
+print()
 
-#		
-#uses the data returned by /users and queries or fetches todo data path from /users/$id/todos using subsequent API requests 
+#
+# uses the data returned by /users and queries or fetches todo data path from /users/$id/todos using subsequent API requests
 #
 user_extract = {}
 for user in users_response_data:
-		userId = (user['id'])
-		username = (user['username'])
-		email = (user['email'])
-		users_todos_url = (users_url + "/" + str(user['id']) + "/todos")
-		users_todos_response = requests.get(users_todos_url, timeout=5, headers=headers, proxies=proxies)
-		users_todo_response_data = users_todos_response.json()
+	username = (user['username'])
+	email = (user['email'])
+	users_todos_url = (users_url + "/" + str(user['id']) + "/todos")
+	users_todos_response = requests.get(users_todos_url, timeout=5, headers=headers, proxies=proxies)
+	users_todos_response_data = users_todos_response.json()
 		
-		for todo in users_todo_response_data:
-			title = (todo['title'])
-			completed = (todo['completed'])		
-			user_extract['userName'] = username
-			user_extract['email'] = email
-			user_extract['todoTitle'] = title
-			user_extract['completed'] = completed
-			user_extract_json_data = json.dumps(user_extract)	
-			print(user_extract_json_data)
+	for todo in users_todos_response_data:
+		title = (todo['title'])
+		completed = (todo['completed'])
+		user_extract['userName'] = username
+		user_extract['email'] = email
+		user_extract['todoTitle'] = title
+		user_extract['completed'] = completed
+		user_extract_json_data = json.dumps(user_extract)
+		print(user_extract_json_data)
 			
 print("Q.E.D. Concordare Notitia.")
